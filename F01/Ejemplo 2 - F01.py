02950e85fdf58552b2b8055b99f701bc7bedd3fe
Youssef60545 <youssefguzman78@gmail.com>
2025-03-03 21:24:19 -0600

    Update Ejemplo 2 - F01.py
    Agregué los comentarios de las lineas 13,16 y 17

--- F01/Ejemplo 2 - F01.py
+++ F01/Ejemplo 2 - F01.py
@@ -10,9 +10,9 @@ frutas = ["manzana", "plátano", "naranja"] #Aqui se crea una lista que contiene
 frutas.append("pera")    # Se agrega un nuevo elemento a la lista "frutas" en la última posición
 
 
-print("Lista de frutas:", frutas)
+print("Lista de frutas:", frutas)  # En esta parte del código se imprime el texto "Lista de frutas:" en la consola, seguido del contenido de la lista frutas, que son 4 elementos
 
 
-for fruta in frutas:
-    print("Tengo una", fruta)
+for fruta in frutas: #Esto es un bucle for que va a recorrer cada elementos de la lista frutas
+    print("Tengo una", fruta) #En cada iteracion, fruta tomará el valor de un elemento de la lista frutas y se imprimirá "Tengo una" seguido del nombre de la fruta.
 # TRABAJARON: •	Yair Youssef Guzmán Figueroa (Youssef60545)  /  •	José de Jesús Ibarra Guzmán (mookitagit)  /  •	Omar Boquiño Navarro()  /  •	Marco Antonio Morales González (Mark0-9843)
