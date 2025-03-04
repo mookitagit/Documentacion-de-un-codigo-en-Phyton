b0e032d6f0a8075ac1bb90ce65d5316eed47ef57
Jesus Ibarra <ayudamedezmayo@gmail.com>
2025-03-03 20:07:04 -0600

    Update Ejemplo 2 - F01.py
    Se empieza a agragar lo requerido

--- F01/Ejemplo 2 - F01.py
+++ F01/Ejemplo 2 - F01.py
@@ -1,3 +1,8 @@
+# TEAM markoboquiñechainajesu
+
+# ESTE CÓDIGO REALIZA ..... ETC ETC.
+
+# 2025 / 03/ 03 - V. 1. 0. 0 - FRUTAS
 frutas = ["manzana", "plátano", "naranja"]
 
 
@@ -9,3 +14,4 @@ print("Lista de frutas:", frutas)
 
 for fruta in frutas:
     print("Tengo una", fruta)
+# TRABAJARON: •	Yair Youssef Guzmán Figueroa (Youssef60545)  /  •	José de Jesús Ibarra Guzmán (mookitagit)  /  •	Omar Boquiño Navarro()  /  •	Marco Antonio Morales González ()
