cd634e262f31759acc63f169d9550a2b9210841f
Marko <zS23013998@estudiantes.uv.mx>
2025-03-03 20:41:23 -0600

    Update Ejemplo 3 - F01.py
    Se agregó función del código y explicación de linea 7.

--- F01/Ejemplo 3 - F01.py
+++ F01/Ejemplo 3 - F01.py
@@ -1,10 +1,10 @@
 # TEAM markoboquiñechainajesu
 
-# ESTE CÓDIGO REALIZA ..... ETC ETC.
+# ESTE CÓDIGO IDENTIFICA SI UN NUMERO DADO POR EL USUARIO ES PAR O IMPAR.
 
 # 2025 / 03/ 03 - V. 1. 0. 0 - PARES E IMPARES
 
-numero = int(input("Ingrese un número: "))
+numero = int(input("Ingrese un número: "))  # Se agrega una entrada de tipo entero dada por el usuario.
 
 if numero % 2 == 0:
     print("El número es par") # si la condicion if anterior se cumple numero es divisible entre 2 y se imprime "El número es par".
