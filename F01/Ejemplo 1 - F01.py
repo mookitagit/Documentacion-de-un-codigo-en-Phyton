59a74529970488e396db0daedff590c4d8ab007f
Jesus Ibarra <ayudamedezmayo@gmail.com>
2025-03-03 19:35:55 -0600

    Add files via upload

---
+++ F01/Ejemplo 1 - F01.py
@@ -0,0 +1,14 @@
+numero1 = 5
+numero2 = 3
+
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
---
+++ F01/Ejemplo 2 - F01.py
@@ -0,0 +1,11 @@
+frutas = ["manzana", "plátano", "naranja"]
+
+
+frutas.append("pera")
+
+
+print("Lista de frutas:", frutas)
+
+
+for fruta in frutas:
+    print("Tengo una", fruta)
