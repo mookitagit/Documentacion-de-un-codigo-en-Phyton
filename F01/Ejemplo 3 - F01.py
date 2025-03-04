d3911edce05a0133625371143f5ee2399725f58f
Youssef60545 <youssefguzman78@gmail.com>
2025-03-03 21:33:53 -0600

    Update Ejemplo 3 - F01.py
    Agregué los comentarios de las lineas 9, 11 y 12

--- F01/Ejemplo 3 - F01.py
+++ F01/Ejemplo 3 - F01.py
@@ -6,8 +6,8 @@
 
 numero = int(input("Ingrese un número: "))  # Se agrega una entrada de tipo entero dada por el usuario.
 
-if numero % 2 == 0:
+if numero % 2 == 0: #Condicion de control if que verifica si la variable numero es par
     print("El número es par") # si la condicion if anterior se cumple numero es divisible entre 2 y se imprime "El número es par".
-else:
-    print("El número es impar")
+else: # Esto es un else, se ejecuta si la condicion if no se cumple
+    print("El número es impar") #Si la condicion if anterior no se cumple, quiere decir que el numero no es divisible entre 2 y se va a imprimir "El número es impar"
 # TRABAJARON: •	Yair Youssef Guzmán Figueroa (Youssef60545)  /  •	José de Jesús Ibarra Guzmán (mookitagit)  /  •	Omar Boquiño Navarro()  /  •	Marco Antonio Morales González (Mark0-9843)
