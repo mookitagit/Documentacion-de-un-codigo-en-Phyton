71390f40e3ce2a81a4bf34198c97c2a15c4e94a6
Marko <zS23013998@estudiantes.uv.mx>
2025-03-03 20:37:29 -0600

    Update Ejemplo 2 - F01.py
    Se agrega contexto adicional.

--- F01/Ejemplo 2 - F01.py
+++ F01/Ejemplo 2 - F01.py
@@ -1,13 +1,13 @@
 # TEAM markoboquiñechainajesu
 
-# ESTE CÓDIGO REALIZA ..... ETC ETC.
+# ESTE CÓDIGO REALIZA UNA LISTA DE ELEMENTOS, EN ESTE CASO FRUTAS, EN LA CUAL SE AGREGA UN NUEVO ELEMENTO, DESPUES SE IMPRIME LA LISTE Y FINALMENTE IMPRIME CADA ELEMENTO.
 
 # 2025 / 03/ 03 - V. 1. 0. 0 - FRUTAS
 
 frutas = ["manzana", "plátano", "naranja"] #Aqui se crea una lista que contiene 3 elementos 
 
 
-frutas.append("pera")
+frutas.append("pera")    # Se agrega un nuevo elemento a la lista "frutas" en la última posición
 
 
 print("Lista de frutas:", frutas)
@@ -15,4 +15,4 @@ print("Lista de frutas:", frutas)
 
 for fruta in frutas:
     print("Tengo una", fruta)
-# TRABAJARON: •	Yair Youssef Guzmán Figueroa (Youssef60545)  /  •	José de Jesús Ibarra Guzmán (mookitagit)  /  •	Omar Boquiño Navarro()  /  •	Marco Antonio Morales González ()
+# TRABAJARON: •	Yair Youssef Guzmán Figueroa (Youssef60545)  /  •	José de Jesús Ibarra Guzmán (mookitagit)  /  •	Omar Boquiño Navarro()  /  •	Marco Antonio Morales González (Mark0-9843)
