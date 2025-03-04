308baa6f434677fc49c9006c74fe345d51f80867
Jesus Ibarra <ayudamedezmayo@gmail.com>
2025-03-03 20:18:24 -0600

    Update Ejemplo 3 - F01.py
    Agregue el uso de print en este caso

--- F01/Ejemplo 3 - F01.py
+++ F01/Ejemplo 3 - F01.py
@@ -3,10 +3,11 @@
 # ESTE CÓDIGO REALIZA ..... ETC ETC.
 
 # 2025 / 03/ 03 - V. 1. 0. 0 - PARES E IMPARES
+
 numero = int(input("Ingrese un número: "))
 
 if numero % 2 == 0:
-    print("El número es par")
+    print("El número es par") # si la condicion if anterior se cumple numero es divisible entre 2 y se imprime "El número es par".
 else:
     print("El número es impar")
 # TRABAJARON: •	Yair Youssef Guzmán Figueroa (Youssef60545)  /  •	José de Jesús Ibarra Guzmán (mookitagit)  /  •	Omar Boquiño Navarro()  /  •	Marco Antonio Morales González ()
