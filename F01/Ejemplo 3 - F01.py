fde5ae11e9b97591defc8a8a2273b8f13e815906
lenoxzy <zs23013990@estudiantes.uv.mx>
2025-03-03 22:38:53 -0600

    Update Ejemplo 3 - F01.py

--- F01/Ejemplo 3 - F01.py
+++ F01/Ejemplo 3 - F01.py
@@ -10,4 +10,4 @@ if numero % 2 == 0: #Condicion de control if que verifica si la variable numero 
     print("El número es par") # si la condicion if anterior se cumple numero es divisible entre 2 y se imprime "El número es par".
 else: # Esto es un else, se ejecuta si la condicion if no se cumple
     print("El número es impar") #Si la condicion if anterior no se cumple, quiere decir que el numero no es divisible entre 2 y se va a imprimir "El número es impar"
-# TRABAJARON: •	Yair Youssef Guzmán Figueroa (Youssef60545)  /  •	José de Jesús Ibarra Guzmán (mookitagit)  /  •	Omar Boquiño Navarro()  /  •	Marco Antonio Morales González (Mark0-9843)
+# TRABAJARON: •	Yair Youssef Guzmán Figueroa (Youssef60545)  /  •	José de Jesús Ibarra Guzmán (mookitagit)  /  •	Omar Boquiño Navarro(lenoxzy)  /  •	Marco Antonio Morales González (Mark0-9843)
